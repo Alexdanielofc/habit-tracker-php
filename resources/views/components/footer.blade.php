90a840992b37929bb17925e982df49c9fe9f4297
ALEX NUNES <alexpayofc@gmail.com>
2026-09-14 23:24:21 -0300

    feat: init components header/footer/layout

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,5 @@
+<footer class="bg-white border-t-2 p-4">
+  <p>
+    Criado por Alex Daniel. O código fonte está no <a href="#" class="underline">GitHub.</a>
+  </p>
+</footer>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,18 +1,7 @@
-<!doctype html>
-<html lang="pt-br">
-  <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>
-      {{config('app.name')}}
-    </title>
-
-    @vite('resources/css/app.css')
-  </head>
-
-  <body>
-    <h1 class="text-3xl font-bold underline">
-      Hello World!
+<x-layout>
+  <main class="py-10">
+    <h1>
+      Veja seu hábitos ganharem vida
     </h1>
-  </body>
-</html>
+  </main>
+</x-layout>
