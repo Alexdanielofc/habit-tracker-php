f1c320124a6e0ea4460dba84eb4f3ad3b2e2a9bc
ALEX NUNES <alexpayofc@gmail.com>
2026-09-14 23:06:18 -0300

    feat: setup tailwind

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,16 +1,18 @@
-<h1>
-    Welcome to the Home Page!
-</h1>
-<p>
-    Olá, {{$name}}
-</p>
-<p>
-    Seus hábitos são:
-</p>
-<ul>
-    @foreach($habits as $itens)
-        <li>
-            {{$itens}}
-        </li>
-    @endforeach
-</ul>
+<!doctype html>
+<html lang="pt-br">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>
+      {{config('app.name')}}
+    </title>
+
+    @vite('resources/css/app.css')
+  </head>
+
+  <body>
+    <h1 class="text-3xl font-bold underline">
+      Hello World!
+    </h1>
+  </body>
+</html>
