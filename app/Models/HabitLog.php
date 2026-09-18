7eefab8e358377a04166f327863fed8a63959e0b
ALEX NUNES <alexpayofc@gmail.com>
2026-09-18 13:12:39 -0300

    feat(models): establish relationships between User, Habit, and HabitLog

--- app/Models/HabitLog.php
+++ app/Models/HabitLog.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class HabitLog extends Model
 {
@@ -11,4 +12,16 @@ class HabitLog extends Model
         'user_id',
         'completed_at',
     ];
+
+    // Um registro de hábito pertence a um usuário
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    //Um registro de hábito pertence a um hábito
+    public function habit(): BelongsTo
+    {
+        return $this->belongsTo(Habit::class);
+    }
 }
